86cc2a15e428b5240a98e70acd518fd2b037ee48
Battlecoob <if20b031@technikum-wien.at>
2022-03-01 18:57:43 +0100

    missing files

---
+++ MonsterTradingCardGame/RouteCommands/Packages/CreatePackageCommand.cs
@@ -0,0 +1,73 @@
+﻿using MonsterTradingCard.Exceptions;
+using MonsterTradingCardGame.Models;
+using SWE1HttpServer.Core.Response;
+using SWE1HttpServer.Core.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame.RouteCommands
+{
+    class CreatePackageCommand : ProtectedRouteCommand
+    {
+        private readonly IRepoManager repoManager;
+        public List<Card> Cards { get; private set; }
+
+        public CreatePackageCommand(IRepoManager repoManager, List<Card> cards)
+        {
+            Cards = cards;
+            this.repoManager = repoManager;
+        }
+
+        public override Response Execute()
+        {
+            var response = new Response();
+
+            if(User.Token != "admin-mtcgToken")
+            {
+                response.StatusCode = StatusCode.Unauthorized;
+                response.Payload = "Only admins can do such actions."; // negativ fall - .bat
+                return response;
+            }
+
+            if(Cards == null)
+            {
+                response.StatusCode = StatusCode.NoContent;
+                response.Payload = "No card ids.";
+                return response;
+            }
+
+            if(Cards.Count == 5)
+            {
+                try
+                {
+                    // check if admin wants to add a card, that already exists - must be catched
+                    repoManager.CardExists(Cards);
+
+                    foreach (Card card in Cards)
+                        repoManager.AddCard(card);
+
+                    repoManager.CreatePackage(Cards);
+
+                    response.StatusCode = StatusCode.Ok;
+                    response.Payload = "Package successfully created.";
+                }
+                catch(CardAlreadyExistsExcpt) // e gibt den pfad aus und nicht den mitgegebenen string
+                {
+                    response.StatusCode = StatusCode.Conflict;
+                    response.Payload = "Card already exists.";
+                    //response.Payload = "Card with the id: " + e + " already exists.";
+                }
+            }
+            else
+            {
+                response.StatusCode = StatusCode.Conflict;
+                response.Payload = "A Package can only be created with five cards.";
+            }
+
+            return response;
+        }
+    }
+}
